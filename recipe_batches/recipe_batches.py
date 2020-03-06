dd7d49a6e55177ae67f8431c29e9c13c6c188c70
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-03-05 20:54:40 -0500

    recipes solved

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,36 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # https://stackoverflow.com/questions/34264710/what-is-the-point-of-floatinf-in-python
+    # None can also be used here
+    possible = float('inf')
+    # If the length of recipes is less than 1, kick out
+    if len(recipe) < 1:
+        return 0
+    # For each component in a recipe
+    for component in recipe:
+        # use the key to get the value for each item in recipe
+        needed = recipe[component]
+        # map through the supplied ingredients, optionally returns 0 if key is not found
+        supplied = ingredients.get(component, 0)
+        # If the supplied ingredient is less than what is needed, return 0. The recipe cannot be made.
+        if supplied < needed:
+            return 0
+        # If we are able to make a recipe, use floor division to get a whole int value
+        abletomake = supplied // needed
+        # recursively rerun individual components
+        possible = min(abletomake, possible)
+
+    # Return the possible amount of recipes
+    return possible
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
