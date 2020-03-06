b7643d4df1a7400ce62c3fd7d2f4c9acb98f47ac
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-03-05 21:23:41 -0500

    rps solved

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,37 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    # Define possible moves
+    moves = [['rock'], ['paper'], ['scissors']]
+
+    # 0 moves returns an empty list
+    if n == 0:
+        return [[]]
+
+    # If only one round, return the base set of moves
+    if n == 1:
+        return moves
+
+    result = []
+    # Recursively call function and countdown from the initial given n value
+    countdown = rock_paper_scissors(n - 1)
+    # Loop through countdown and use the move set from the next series
+    for inside in countdown:
+        # For every move in moves
+        for move in moves:
+            # a possible move is created by concatenating the inner list with the move
+            possiblemove = inside + move
+            # append the possible move(s) to the result of our function
+            result.append(possiblemove)
+
+    return result
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
